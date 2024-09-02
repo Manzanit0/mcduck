709400e9bed389b83f2d65f329b4629a8bf18b99
Javier García <unico.javier@gmail.com>
2024-09-02 11:15:12 +0200

    Increase timeout window

--- pkg/xhttp/xhttp.go
+++ pkg/xhttp/xhttp.go
@@ -8,13 +8,15 @@ import (
 )
 
 func NewClient() *http.Client {
+	// FIXME: these large timeouts are because some services take a long time in
+	// their requests; these shouldn't be the defaults for every service though.
 	h := http.Client{
-		Timeout: 30 * time.Second,
+		Timeout: 120 * time.Second,
 		Transport: &http.Transport{
 			MaxConnsPerHost:       100,
 			MaxIdleConnsPerHost:   10,
-			IdleConnTimeout:       30 * time.Second,
-			ResponseHeaderTimeout: 30 * time.Second,
+			IdleConnTimeout:       120 * time.Second,
+			ResponseHeaderTimeout: 120 * time.Second,
 		},
 	}
 
