0929eb2a63f1a2238b3186d050c48c9eb160cbc9
Javier García <unico.javier@gmail.com>
2024-10-02 09:39:06 +0200

    Extract more HTML into subcomponents

--- spidey/islands/SearchableTable.tsx
+++ spidey/islands/SearchableTable.tsx
@@ -59,168 +59,8 @@ export default function SearcheableTable(props: TableProps) {
   return (
     <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
       <div class="flex flex-column sm:flex-row flex-wrap space-y-4 sm:space-y-0 items-center justify-between pb-4">
-        <div>
-          <button
-            id="dropdownRadioButton"
-            data-dropdown-toggle="dropdownRadio"
-            class="inline-flex items-center text-gray-500 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-3 py-1.5 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
-            type="button"
-          >
-            <svg
-              class="w-3 h-3 text-gray-500 dark:text-gray-400 me-3"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-            >
-              <path d="M10 0a10 10 0 1 0 10 10A10.011 10.011 0 0 0 10 0Zm3.982 13.982a1 1 0 0 1-1.414 0l-3.274-3.274A1.012 1.012 0 0 1 9 10V6a1 1 0 0 1 2 0v3.586l2.982 2.982a1 1 0 0 1 0 1.414Z" />
-            </svg>
-            Last 30 days
-            <svg
-              class="w-2.5 h-2.5 ms-2.5"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 10 6"
-            >
-              <path
-                stroke="currentColor"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="m1 1 4 4 4-4"
-              />
-            </svg>
-          </button>
-          <div
-            id="dropdownRadio"
-            class="z-10 hidden w-48 bg-white divide-y divide-gray-100 rounded-lg shadow dark:bg-gray-700 dark:divide-gray-600"
-            data-popper-reference-hidden=""
-            data-popper-escaped=""
-            data-popper-placement="top"
-            style="position: absolute; inset: auto auto 0px 0px; margin: 0px; transform: translate3d(522.5px, 3847.5px, 0px);"
-          >
-            <ul
-              class="p-3 space-y-1 text-sm text-gray-700 dark:text-gray-200"
-              aria-labelledby="dropdownRadioButton"
-            >
-              <li>
-                <div class="flex items-center p-2 rounded hover:bg-gray-100 dark:hover:bg-gray-600">
-                  <input
-                    id="filter-radio-example-1"
-                    type="radio"
-                    value=""
-                    name="filter-radio"
-                    class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-                  />
-                  <label
-                    for="filter-radio-example-1"
-                    class="w-full ms-2 text-sm font-medium text-gray-900 rounded dark:text-gray-300"
-                  >
-                    Last day
-                  </label>
-                </div>
-              </li>
-              <li>
-                <div class="flex items-center p-2 rounded hover:bg-gray-100 dark:hover:bg-gray-600">
-                  <input
-                    checked={true}
-                    id="filter-radio-example-2"
-                    type="radio"
-                    value=""
-                    name="filter-radio"
-                    class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-                  />
-                  <label
-                    for="filter-radio-example-2"
-                    class="w-full ms-2 text-sm font-medium text-gray-900 rounded dark:text-gray-300"
-                  >
-                    Last 7 days
-                  </label>
-                </div>
-              </li>
-              <li>
-                <div class="flex items-center p-2 rounded hover:bg-gray-100 dark:hover:bg-gray-600">
-                  <input
-                    id="filter-radio-example-3"
-                    type="radio"
-                    value=""
-                    name="filter-radio"
-                    class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-                  />
-                  <label
-                    for="filter-radio-example-3"
-                    class="w-full ms-2 text-sm font-medium text-gray-900 rounded dark:text-gray-300"
-                  >
-                    Last 30 days
-                  </label>
-                </div>
-              </li>
-              <li>
-                <div class="flex items-center p-2 rounded hover:bg-gray-100 dark:hover:bg-gray-600">
-                  <input
-                    id="filter-radio-example-4"
-                    type="radio"
-                    value=""
-                    name="filter-radio"
-                    class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-                  />
-                  <label
-                    for="filter-radio-example-4"
-                    class="w-full ms-2 text-sm font-medium text-gray-900 rounded dark:text-gray-300"
-                  >
-                    Last month
-                  </label>
-                </div>
-              </li>
-              <li>
-                <div class="flex items-center p-2 rounded hover:bg-gray-100 dark:hover:bg-gray-600">
-                  <input
-                    id="filter-radio-example-5"
-                    type="radio"
-                    value=""
-                    name="filter-radio"
-                    class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-                  />
-                  <label
-                    for="filter-radio-example-5"
-                    class="w-full ms-2 text-sm font-medium text-gray-900 rounded dark:text-gray-300"
-                  >
-                    Last year
-                  </label>
-                </div>
-              </li>
-            </ul>
-          </div>
-        </div>
-        <label for="table-search" class="sr-only">
-          Search
-        </label>
-        <div class="relative">
-          <div class="absolute inset-y-0 left-0 rtl:inset-r-0 rtl:right-0 flex items-center ps-3 pointer-events-none">
-            <svg
-              class="w-5 h-5 text-gray-500 dark:text-gray-400"
-              aria-hidden="true"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                fill-rule="evenodd"
-                d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
-                clip-rule="evenodd"
-              >
-              </path>
-            </svg>
-          </div>
-          <input
-            onInput={filterReceipts}
-            type="text"
-            id="table-search"
-            class="block p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg w-80 bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            placeholder="Search for items"
-          />
-        </div>
+        <DateFilterDropdown />
+        <SearchBox onInput={filterReceipts} />
       </div>
       <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
         <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
@@ -296,10 +136,12 @@ function row(r: Signal<ViewReceipt>, url: string) {
 
     r.value = { ...r.value, date: value };
 
-    await updateReceipt(url, { id: r.peek().id, date: Timestamp.fromDate(new Date(value)) });
+    await updateReceipt(url, {
+      id: r.peek().id,
+      date: Timestamp.fromDate(new Date(value)),
+    });
     console.log("updated date to", value);
   };
-
 
   return (
     <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
@@ -577,6 +419,7 @@ interface DatepickerProps {
   value: string;
   onChange: (e: JSX.TargetedEvent<HTMLInputElement>) => Promise<void>;
 }
+
 function DatePicker(props: DatepickerProps) {
   return (
     <div>
@@ -588,6 +431,184 @@ function DatePicker(props: DatepickerProps) {
           value={props.value.split("T")[0]}
           onChange={props.onChange}
         />
+      </div>
+    </div>
+  );
+}
+
+interface SearchBoxProps {
+  onInput: (e: JSX.TargetedEvent<HTMLInputElement>) => void;
+}
+
+function SearchBox({ onInput }: SearchBoxProps) {
+  return (
+    <>
+      <label for="table-search" class="sr-only">
+        Search
+      </label>
+      <div class="relative">
+        <div class="absolute inset-y-0 left-0 rtl:inset-r-0 rtl:right-0 flex items-center ps-3 pointer-events-none">
+          <svg
+            class="w-5 h-5 text-gray-500 dark:text-gray-400"
+            aria-hidden="true"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
+              clip-rule="evenodd"
+            >
+            </path>
+          </svg>
+        </div>
+        <input
+          onInput={onInput}
+          type="text"
+          id="table-search"
+          class="block p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg w-80 bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          placeholder="Search for items"
+        />
+      </div>
+    </>
+  );
+}
+
+function DateFilterDropdown() {
+  return (
+    <div>
+      <button
+        id="dropdownRadioButton"
+        data-dropdown-toggle="dropdownRadio"
+        class="inline-flex items-center text-gray-500 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-3 py-1.5 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
+        type="button"
+      >
+        <svg
+          class="w-3 h-3 text-gray-500 dark:text-gray-400 me-3"
+          aria-hidden="true"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="currentColor"
+          viewBox="0 0 20 20"
+        >
+          <path d="M10 0a10 10 0 1 0 10 10A10.011 10.011 0 0 0 10 0Zm3.982 13.982a1 1 0 0 1-1.414 0l-3.274-3.274A1.012 1.012 0 0 1 9 10V6a1 1 0 0 1 2 0v3.586l2.982 2.982a1 1 0 0 1 0 1.414Z" />
+        </svg>
+        Last 30 days
+        <svg
+          class="w-2.5 h-2.5 ms-2.5"
+          aria-hidden="true"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 10 6"
+        >
+          <path
+            stroke="currentColor"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="m1 1 4 4 4-4"
+          />
+        </svg>
+      </button>
+      <div
+        id="dropdownRadio"
+        class="z-10 hidden w-48 bg-white divide-y divide-gray-100 rounded-lg shadow dark:bg-gray-700 dark:divide-gray-600"
+        data-popper-reference-hidden=""
+        data-popper-escaped=""
+        data-popper-placement="top"
+        style="position: absolute; inset: auto auto 0px 0px; margin: 0px; transform: translate3d(522.5px, 3847.5px, 0px);"
+      >
+        <ul
+          class="p-3 space-y-1 text-sm text-gray-700 dark:text-gray-200"
+          aria-labelledby="dropdownRadioButton"
+        >
+          <li>
+            <div class="flex items-center p-2 rounded hover:bg-gray-100 dark:hover:bg-gray-600">
+              <input
+                id="filter-radio-example-1"
+                type="radio"
+                value=""
+                name="filter-radio"
+                class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+              />
+              <label
+                for="filter-radio-example-1"
+                class="w-full ms-2 text-sm font-medium text-gray-900 rounded dark:text-gray-300"
+              >
+                Last day
+              </label>
+            </div>
+          </li>
+          <li>
+            <div class="flex items-center p-2 rounded hover:bg-gray-100 dark:hover:bg-gray-600">
+              <input
+                checked={true}
+                id="filter-radio-example-2"
+                type="radio"
+                value=""
+                name="filter-radio"
+                class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+              />
+              <label
+                for="filter-radio-example-2"
+                class="w-full ms-2 text-sm font-medium text-gray-900 rounded dark:text-gray-300"
+              >
+                Last 7 days
+              </label>
+            </div>
+          </li>
+          <li>
+            <div class="flex items-center p-2 rounded hover:bg-gray-100 dark:hover:bg-gray-600">
+              <input
+                id="filter-radio-example-3"
+                type="radio"
+                value=""
+                name="filter-radio"
+                class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+              />
+              <label
+                for="filter-radio-example-3"
+                class="w-full ms-2 text-sm font-medium text-gray-900 rounded dark:text-gray-300"
+              >
+                Last 30 days
+              </label>
+            </div>
+          </li>
+          <li>
+            <div class="flex items-center p-2 rounded hover:bg-gray-100 dark:hover:bg-gray-600">
+              <input
+                id="filter-radio-example-4"
+                type="radio"
+                value=""
+                name="filter-radio"
+                class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+              />
+              <label
+                for="filter-radio-example-4"
+                class="w-full ms-2 text-sm font-medium text-gray-900 rounded dark:text-gray-300"
+              >
+                Last month
+              </label>
+            </div>
+          </li>
+          <li>
+            <div class="flex items-center p-2 rounded hover:bg-gray-100 dark:hover:bg-gray-600">
+              <input
+                id="filter-radio-example-5"
+                type="radio"
+                value=""
+                name="filter-radio"
+                class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+              />
+              <label
+                for="filter-radio-example-5"
+                class="w-full ms-2 text-sm font-medium text-gray-900 rounded dark:text-gray-300"
+              >
+                Last year
+              </label>
+            </div>
+          </li>
+        </ul>
       </div>
     </div>
   );
