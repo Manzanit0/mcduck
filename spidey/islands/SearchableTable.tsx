49396ddcdc8a8c7a7e6d6f226afbbc6ac53dbf23
Javier García <unico.javier@gmail.com>
2024-10-02 14:19:32 +0200

    Make searchbar focus colour gray instead of blue

--- spidey/islands/SearchableTable.tsx
+++ spidey/islands/SearchableTable.tsx
@@ -437,7 +437,7 @@ function SearchBox({ onInput }: SearchBoxProps) {
           onInput={onInput}
           type="text"
           id="table-search"
-          class="block p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg w-80 bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          class="block p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg w-80 bg-gray-50 focus:ring-gray-500 focus:border-gray-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-gray-500 dark:focus:border-gray-500"
           placeholder="Search for items"
         />
       </div>
