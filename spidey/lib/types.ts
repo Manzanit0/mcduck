028a4e8beb7b1cd04bf467008f880b9711d6ee0d
Javier García <unico.javier@gmail.com>
2024-10-03 18:32:26 +0200

    Add expenses table

--- spidey/lib/types.ts
+++ spidey/lib/types.ts
@@ -1,39 +1,47 @@
-import { Receipt } from "../gen/receipts.v1/receipts_pb.ts";
+import { Expense, Receipt } from "../gen/receipts.v1/receipts_pb.ts";
 
 export interface SerializableReceipt {
-    id: bigint;
-    status: string;
-    vendor: string;
-    date?: string;
-    expenses: SerializableExpense[];
+  id: bigint;
+  status: string;
+  vendor: string;
+  date?: string;
+  expenses: SerializableExpense[];
 }
 
 export interface SerializableExpense {
-    id: bigint;
-    date?: string;
-    category: string;
-    subcategory: string;
-    description: string;
-    amount: bigint;
+  id: bigint;
+  date?: string;
+  category: string;
+  subcategory: string;
+  description: string;
+  amount: bigint;
 }
 
-export function mapReceiptsToSerializable(receipts: Receipt[]): SerializableReceipt[] {
-    return receipts.map((r) => {
-        return {
-            id: r.id,
-            status: r.status.toString(),
-            vendor: r.vendor,
-            date: r.date?.toDate().toISOString(),
-            expenses: r.expenses.map((e) => {
-                return {
-                    id: e.id,
-                    date: e.date?.toDate().toISOString(),
-                    category: e.category,
-                    subcategory: e.subcategory,
-                    description: e.description,
-                    amount: e.amount,
-                };
-            }),
-        };
-    });
+export function mapReceiptsToSerializable(
+  receipts: Receipt[],
+): SerializableReceipt[] {
+  return receipts.map((r) => {
+    return {
+      id: r.id,
+      status: r.status.toString(),
+      vendor: r.vendor,
+      date: r.date?.toDate().toISOString(),
+      expenses: mapExpensesToSerializable(r.expenses),
+    };
+  });
+}
+
+export function mapExpensesToSerializable(
+  expenses: Expense[],
+): SerializableExpense[] {
+  return expenses.map((e) => {
+    return {
+      id: e.id,
+      date: e.date?.toDate().toISOString(),
+      category: e.category,
+      subcategory: e.subcategory,
+      description: e.description,
+      amount: e.amount,
+    };
+  });
 }
