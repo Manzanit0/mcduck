363fa73a750fdb0b927ec7cbe0a64dbdddb8eea0
Javier García <unico.javier@gmail.com>
2024-10-03 11:26:02 +0200

    Extract textinput to components

---
+++ spidey/components/TextInput.tsx
@@ -0,0 +1,23 @@
+import { JSX } from "preact";
+
+interface TextInputProps {
+  value: string;
+  onfocusout: (e: JSX.TargetedEvent<HTMLInputElement>) => Promise<void>;
+}
+
+export default function TextInput(props: TextInputProps) {
+  return (
+    <div>
+      <div class="relative mt-2 rounded-md shadow-sm">
+        <input
+          type="text"
+          class="block w-full rounded-md border-0 text-gray-900 ring-1 ring-inset ring-gray-300 sm:text-sm sm:leading-6 focus:outline-none focus:ring-2 focus:ring-gray-500"
+          placeholder="0.00"
+          value={props.value}
+          onfocusout={props.onfocusout}
+        />
+      </div>
+    </div>
+  );
+}
+
