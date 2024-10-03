3b6ed6af4bbd784dfac6a6f87c3e8c19360e3a0b
Javier García <unico.javier@gmail.com>
2024-10-03 11:26:02 +0200

    Extract textinput to components

--- spidey/islands/ReceiptsTable.tsx
+++ spidey/islands/ReceiptsTable.tsx
@@ -4,6 +4,7 @@ import { JSX } from "preact/jsx-runtime";
 import { SerializableReceipt } from "../lib/types.ts";
 import { updateReceipt } from "../lib/receipts.ts";
 import { Timestamp } from "@bufbuild/protobuf";
+import TextInput from "../components/TextInput.tsx";
 
 interface TableProps {
   receipts: SerializableReceipt[];
@@ -360,27 +361,6 @@ function na(selected: boolean) {
       </div>
       {selected ? checkmark() : <></>}
     </>
-  );
-}
-
-interface TextInputProps {
-  value: string;
-  onfocusout: (e: JSX.TargetedEvent<HTMLInputElement>) => Promise<void>;
-}
-
-function TextInput(props: TextInputProps) {
-  return (
-    <div>
-      <div class="relative mt-2 rounded-md shadow-sm">
-        <input
-          type="text"
-          class="block w-full rounded-md border-0 text-gray-900 ring-1 ring-inset ring-gray-300 sm:text-sm sm:leading-6 focus:outline-none focus:ring-2 focus:ring-gray-500"
-          placeholder="0.00"
-          value={props.value}
-          onfocusout={props.onfocusout}
-        />
-      </div>
-    </div>
   );
 }
 
